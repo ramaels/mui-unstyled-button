655e5f5abfc16c7c8d5853b33967bc54a6cae0bc
Infinite Spiral <51630833+ramaels@users.noreply.github.com>
2022-08-28 12:11:39 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import Demo from './demo';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>
+);
